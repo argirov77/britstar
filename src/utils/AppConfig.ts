43810ac53c9fa6ee90d65070d4de543e56009b4e
argirov77 <164051115+argirov77@users.noreply.github.com>
2025-06-23 22:18:16 +0200

    Fix linting and build

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
   site_name: 'BRITSTAR MANAGEMENT LIMITED',
   title: 'Performance Marketing & Affiliate Agency',
-  description: 'Specializing in affiliate marketing, Google Ads and Facebook Ads to drive targeted traffic and measurable growth.',
+  description:
+    'Specializing in affiliate marketing, Google Ads and Facebook Ads to drive targeted traffic and measurable growth.',
   locale: 'en',
 };
